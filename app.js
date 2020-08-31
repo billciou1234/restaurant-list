997fd5880210ab5fb59c41d1dfc883b5b6a7f121
Bill <reborn800201@gmail.com>
2020-08-31 23:31:29 +0800

    project in

---
+++ app.js
@@ -0,0 +1,45 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+
+//join restaurants list
+const restaurantList = require('./restaurant.json')
+
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurant: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.filter(function (restaurant) {
+    return restaurant.id === Number(req.params.restaurant_id)
+  })
+  res.render('show', { restaurant: restaurant[0] })
+})
+
+app.get('/search', (req, res) => {
+  const restaurants = restaurantList.results.filter(function (restaurant) {
+    return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase())
+  })
+
+
+  res.render('index', { restaurant: restaurants, keyword: req.query.keyword })
+
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
+
+
+
