9c9bd892fa6b8dcded7a8ec221565df0ca4358f1
Bill <reborn800201@gmail.com>
2020-09-18 17:18:45 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantseeder.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongdb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+const Restaurant = require('../restaurant')
+const restaurantList = require('../../restaurant.json')
+
+console.log(restaurantList.results[0].name)
+
+db.once('open', () => {
+  for (let i = 0; i < restaurantList.results.length; i++) {
+    Restaurant.create({
+      id: restaurantList.results[i].id,
+      name: restaurantList.results[i].name,
+      name_en: restaurantList.results[i].name_en,
+      category: restaurantList.results[i].category,
+      image: restaurantList.results[i].image,
+      location: restaurantList.results[i].location,
+      phone: restaurantList.results[i].phone,
+      google_map: restaurantList.results[i].google_map,
+      rating: restaurantList.results[i].rating,
+      description: restaurantList.results[i].description
+    })
+  }
+  console.log('done.')
+})
+
+  // , name: restaurantList.results[i].name, name_en: restaurantList.results[i].name_en, category: restaurantList.results[i].category, image: restaurantList.results[i].image, local: restaurantList.results[i].local, phone: restaurantList.results[i].phone, google_map: restaurantList.results[i].google_map, rating: restaurantList.results[i].rating, description: restaurantList.results[i].description
